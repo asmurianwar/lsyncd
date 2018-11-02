66fc5138eff66984e3cf5ba13db25a26477f2eef
Asmuri Anwar <asmuri.anwar@gmail.com>
2018-11-02 11:37:44 +0700

    Create lsyncd.conf.lua

---
+++ lsyncd.conf.lua
@@ -0,0 +1,34 @@
+dirs    = { "tn", "pd", "apis"  }
+sources = { "/db/backup/daily", "/home" }
+
+local source, target
+
+for _, dir in ipairs(dirs) do
+	for s, src in ipairs(sources) do
+		if s == 2 then
+			source = src .. '/' .. dir .. '/www'
+			if dir == 'apis' then
+				target = "/backupdrive/pams/services/s2/" .. dir
+			else
+				target = "/backupdrive/pams/" .. dir .. '/app'
+			end
+		else
+			if dir ~= 'apis' then
+				if dir == 'pd' then
+					source = src .. '/pede'
+				else
+					source = src .. '/' .. dir
+				end
+				target = "/backupdrive/pams/" .. dir .. '/db'
+			else
+				source = nil
+				target = nil
+			end
+		end
+
+		if target ~= nil and source ~= nil then
+			print (source .. ' --> ' .. target)
+      -- sync { here }
+		end
+	end
+end
